7b5330509214e8044b9cc023e97865944a77201d
rodrigoromano23 <rr699785@gmail.com>
2025-12-14 23:59:56 -0300

    feat

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173", // puerto donde corre Vite/React
+  origin: "https://frontend-romano.onrender.com/", // puerto donde corre Vite/React
   credentials: true
 }));
 app.use(express.json({ limit: "20mb" }));
